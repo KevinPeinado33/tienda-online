3e0117753671794b7633d4475e5f777ce505e484
kpeinado <kpeinado@everis.com>
2021-10-22 22:58:46 -0500

    redux preparado

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,24 @@ import {
   Redirect
 } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+
 import { HomePage } from './components/pages/HomePage';
 import { MyPublicationPage } from './components/pages/MyPublicationPage';
+
+import store from './stateManagement/store';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path='/home' exact component={ HomePage } />
-        <Route path='/my-publications' exact component={ MyPublicationPage } />
-        
-        <Redirect from='/' to='home' />
-      </Switch>
+      <Provider store={ store }>
+        <Switch>
+          <Route path='/home' exact component={ HomePage } />
+          <Route path='/my-publications' exact component={ MyPublicationPage } />
+          
+          <Redirect from='/' to='home' />
+        </Switch>
+      </Provider>
     </Router>
   )
 }
