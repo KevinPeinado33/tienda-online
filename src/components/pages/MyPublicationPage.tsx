76c1507b88f2f38454ddc88dd0e4c393192b71b8
kpeinado <kpeinado@everis.com>
2021-10-30 00:07:37 -0500

    subiendo

--- src/components/pages/MyPublicationPage.tsx
+++ src/components/pages/MyPublicationPage.tsx
@@ -1,7 +1,42 @@
+import { Card, Form , Row, Col } from 'react-bootstrap';
+
 export const MyPublicationPage = () => {
     return (
         <div className="mt-4">
-            <h2>MyPublicationPage</h2>
+            <h2>Mis Publicaciones</h2>
+            <Card className='mt-4'>
+                <Card.Body>
+                    <Form>
+                        <Row>
+                            <Col sm>
+                                <Form.Group className='mb-3'>
+                                    <Form.Label>Titulo</Form.Label>
+                                    <Form.Control 
+                                        type='text' 
+                                        placeholder='Se intercambia o vende ...' />
+                                </Form.Group>
+                            </Col>
+                            <Col sm>
+                                <Form.Group className='mb-3'>
+                                    <Form.Label>Precio</Form.Label>
+                                    <Form.Control 
+                                        type='text' 
+                                        placeholder='0.0' />
+                                </Form.Group>
+                            </Col>
+                            <Col sm>
+                                <Form.Group className='mb-3'>
+                                    <Form.Label>Descripción</Form.Label>
+                                    <Form.Control 
+                                        as='textarea'
+                                        rows={3}
+                                        placeholder='Breve descripción.' />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                    </Form>
+                </Card.Body>
+            </Card>
         </div>
     )
 }
