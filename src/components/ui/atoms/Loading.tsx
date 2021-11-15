4f898aba03ade4f1d3d805a4509e68ac3bca49c3
kpeinado <kpeinado@everis.com>
2021-11-15 16:05:39 -0500

    .

--- src/components/ui/atoms/Loading.tsx
+++ src/components/ui/atoms/Loading.tsx
@@ -1,1 +1,5 @@
-export const Loading = () => ( <h5 className="mt-4 mb-4 text-center">Cargando ...</h5> );
+export const Loading = () => ( 
+    <h5 className="mt-4 mb-4 text-center">
+        Cargando ...
+    </h5> 
+);
